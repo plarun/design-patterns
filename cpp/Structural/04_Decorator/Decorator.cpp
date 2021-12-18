9acd4b9788a3effa647f3bd03b0863193f006130
plarun <arunpandipava@gmail.com>
2021-12-18 21:38:54 +0530

    update

--- cpp/Structural/04_Decorator/Decorator.cpp
+++ cpp/Structural/04_Decorator/Decorator.cpp
@@ -15,125 +15,125 @@ Use the pattern when it’s awkward or not possible to extend an object’s beha
 
 // interface
 class DataSource {
-	public:
-		virtual void writeData(std::string data) = 0;
-		virtual std::string readData() = 0;
+public:
+	virtual void writeData(std::string data) = 0;
+	virtual std::string readData() = 0;
 };
 
 class FileDataSource : public DataSource {
-	private:
-		std::string filename;
+private:
+	std::string filename;
 
-	public:
-		FileDataSource(std::string name) : filename(name) {}
-		void writeData(std::string data) override {
-			std::ofstream fout;
-			fout.open(filename);
-			fout << data;
-			fout.close();
-			std::cout << "write: " << data << std::endl;
-		}
-		std::string readData() override {
-			std::string data, line;
-			std::ifstream fin;
-			fin.open(filename);
-			fin >> line;
-			data += line;
-			fin.close();
-			std::cout << "read: " << data << std::endl;
-			return data;
-		}
+public:
+	FileDataSource(std::string name) : filename(name) {}
+	void writeData(std::string data) override {
+		std::ofstream fout;
+		fout.open(filename);
+		fout << data;
+		fout.close();
+		std::cout << "write: " << data << std::endl;
+	}
+	std::string readData() override {
+		std::string data, line;
+		std::ifstream fin;
+		fin.open(filename);
+		fin >> line;
+		data += line;
+		fin.close();
+		std::cout << "read: " << data << std::endl;
+		return data;
+	}
 };
 
 class DataSourceDecorator : public DataSource {
-	private:
-		DataSource* wrappee;
-	
-	public:
-		DataSourceDecorator(DataSource* source) : wrappee(source) {}
-		void writeData(std::string data) override {
-			wrappee->writeData(data);
-		}
-		std::string readData() override {
-			return wrappee->readData();
-		}
+private:
+	DataSource* wrappee;
+
+public:
+	DataSourceDecorator(DataSource* source) : wrappee(source) {}
+	void writeData(std::string data) override {
+		wrappee->writeData(data);
+	}
+	std::string readData() override {
+		return wrappee->readData();
+	}
 };
 
 class EncryptionDecorator : public DataSourceDecorator {
-	private:
-		std::string encrypt(std::string data) {
-			std::string cipherText;
-			int key = 15;
-			for (int c: data)
-				cipherText += char((int(c+key-'a')%26)+'a');
-			std::cout << "encrypt: " << cipherText << std::endl;
-			return cipherText;
-		}
-		std::string decrypt(std::string data) {
-			std::string normalText;
-			int key = 15;
-			for (char& c: data)
-				normalText += char((int(c-key-'a')%26)+'a');
-			std::cout << "decrypt: " << normalText << std::endl;
-			return normalText;
-		}
-	public:
-		EncryptionDecorator(DataSource* source) : DataSourceDecorator(source) {}
-		void writeData(std::string data) override {
-			DataSourceDecorator::writeData(encrypt(data));
-		}
-		std::string readData() override {
-			return decrypt(DataSourceDecorator::readData());
-		}
+private:
+	std::string encrypt(std::string data) {
+		std::string cipherText;
+		int key = 15;
+		for (int c: data)
+			cipherText += char((int(c+key-'a')%26)+'a');
+		std::cout << "encrypt: " << cipherText << std::endl;
+		return cipherText;
+	}
+	std::string decrypt(std::string data) {
+		std::string normalText;
+		int key = 15;
+		for (char& c: data)
+			normalText += char((int(c-key-'a')%26)+'a');
+		std::cout << "decrypt: " << normalText << std::endl;
+		return normalText;
+	}
+public:
+	EncryptionDecorator(DataSource* source) : DataSourceDecorator(source) {}
+	void writeData(std::string data) override {
+		DataSourceDecorator::writeData(encrypt(data));
+	}
+	std::string readData() override {
+		return decrypt(DataSourceDecorator::readData());
+	}
 };
 
 class CompressionDecorator : public DataSourceDecorator {
-	private:
-		std::string compress(std::string data) {
-			std::string compressedText;
-			if (data.size()) {
-				for (int i=1, count=0, curr=data[0]; i<=data.size(); ++i) {
-					if (i == data.size() || curr != data[i]) {
-						if (count > 0) compressedText += std::to_string(count+1);
-						compressedText += curr;
-						curr = data[i];
-						count = 0;
-					} else {
-						++count;
-					}
+private:
+	std::string compress(std::string data) {
+		std::string compressedText;
+		if (data.size()) {
+			for (int i=1, count=0, curr=data[0]; i<=data.size(); ++i) {
+				if (i == data.size() || curr != data[i]) {
+					if (count > 0) compressedText += std::to_string(count+1);
+					compressedText += curr;
+					curr = data[i];
+					count = 0;
+				} else {
+					++count;
 				}
 			}
-			std::cout << "compress: " << compressedText << std::endl;
-			return compressedText.size() < data.size() ? compressedText : data;
 		}
-		std::string decompress(std::string data) {
-			std::string normalText;
-			std::string::size_type sz;
-			if (data.size()) {
-				std::string num;
-				for (char& c: data) {
-					if (c-'0' >= 0 && c-'0' <= 9)
-						num += c;
-					else {
-						int n = std::stoi(num, &sz);
-						n = n == 0 ? 1 : n;
-						for (int i=0; i<n; ++i) normalText += c;
-						num = "0";
-					}
+		std::cout << "compress: " << compressedText << std::endl;
+		return compressedText.size() < data.size() ? compressedText : data;
+	}
+	std::string decompress(std::string data) {
+		std::string normalText;
+		std::string::size_type sz;
+		if (data.size()) {
+			std::string num;
+			for (char& c: data) {
+				if (c-'0' >= 0 && c-'0' <= 9)
+					num += c;
+				else {
+					int n = std::stoi(num, &sz);
+					n = n == 0 ? 1 : n;
+					for (int i=0; i<n; ++i) normalText += c;
+					num = "0";
 				}
 			}
-			std::cout << "decompress: " << normalText << std::endl;
-			return normalText;
 		}
-	
-	public:
-		CompressionDecorator(DataSource* source) : DataSourceDecorator(source) {}
-		void writeData(std::string data) override {
-			DataSourceDecorator::writeData(compress(data));
-		}
-		std::string readData() override {
-			return decompress(DataSourceDecorator::readData());
-		}
+		std::cout << "decompress: " << normalText << std::endl;
+		return normalText;
+	}
+
+public:
+	CompressionDecorator(DataSource* source) : DataSourceDecorator(source) {}
+	void writeData(std::string data) override {
+		DataSourceDecorator::writeData(compress(data));
+	}
+	std::string readData() override {
+		return decompress(DataSourceDecorator::readData());
+	}
 };
 
 void app() {
